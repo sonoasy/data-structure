c46ba129bbf7f916832f789e3d94206b8ca2451a
sonoasy <61787171+sonoasy@users.noreply.github.com>
2021-02-20 14:03:47 +0900

    random number in range(min,max)

--- random.c
+++ random.c
@@ -1,12 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#define min 30
+#define max 100
+
 int main(void) {
     int  i;
     //시드값 시간에따라 변경
     srand(time(NULL));
     
     for (i = 0; i < 5; i++)
-        printf("%d %d\n", i, rand());
+        printf("%d %d\n", i, rand()%(max-min+1)+min);
     return 0;
 }
+
